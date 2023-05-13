88dc276eca2659b433f2f1a69e8c9db6c35f9b72
Richard Oh <richard@magic.link>
2023-05-13 14:19:35 -0500

    optimize app useeffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { magic } from "./libs/magic";
 import Header from "./components/Header";
@@ -7,21 +7,21 @@ import Protected from "./components/Protected";
 import "./App.css";
 
 function App() {
-  const { user, isAuthenticated, getIdTokenClaims } = useAuth0();
+  const { isAuthenticated, getIdTokenClaims } = useAuth0();
 
-  useEffect(() => {
-    if (isAuthenticated) {
-      connectWallet();
-    }
-  }, [user]);
-
-  const connectWallet = async () => {
+  const connectWallet = useCallback(async () => {
     const token = await getIdTokenClaims();
     await magic.openid.loginWithOIDC({
       jwt: token.__raw,
       providerId: process.env.REACT_APP_MAGIC_PROVIDER_ID,
     });
-  };
+  }, [getIdTokenClaims]);
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      connectWallet();
+    }
+  }, [connectWallet, isAuthenticated]);
 
   return (
     <div className="App">
