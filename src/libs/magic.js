23465e1675e2a179f7e59fbd29a8b74a24c0dd70
RJO <84942969+ayv8er@users.noreply.github.com>
2023-05-26 14:25:57 -0500

    Update magic.js

--- src/libs/magic.js
+++ src/libs/magic.js
@@ -4,7 +4,6 @@ import { OpenIdExtension } from "@magic-ext/oidc";
 export const magic = new Magic(
   process.env.REACT_APP_MAGIC_PUBLISHABLE_API_KEY,
   {
-    network: "goerli",
     extensions: [new OpenIdExtension()],
   }
 );
