0b573033af513619c9662321faf8a3cc4ff2167d
Richard Oh <richard@magic.link>
2024-04-04 22:26:28 -0500

    updates magic and ext

--- src/libs/magic.js
+++ src/libs/magic.js
@@ -2,7 +2,7 @@ import { Magic } from "magic-sdk";
 import { OpenIdExtension } from "@magic-ext/oidc";
 
 export const magic = new Magic(
-  process.env.REACT_APP_MAGIC_PUBLISHABLE_API_KEY,
+  'pk_live_CBE04864892D2CA8',
   {
     extensions: [new OpenIdExtension()],
   }
